f4a7b1a7f5299d31f3ff5f5e1785c1242ee5b221
noobsohail <sikhoshikaay@gmail.com>
2021-08-13 12:35:39 +0530

    dix

--- KurisuMakise/modules/stickers.py
+++ KurisuMakise/modules/stickers.py
@@ -12,7 +12,7 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from KurisuMakiseimport dispatcher
+from KurisuMakise import dispatcher
 from ErzaScarlet.modules.disable import DisableAbleCommandHandler
 
 
